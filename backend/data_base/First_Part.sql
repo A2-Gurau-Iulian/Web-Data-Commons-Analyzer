d1d79e72e2e6ed5cc790bbfa1b57ef8944003fa3
GurauIulian <79217309+A2-Gurau-Iulian@users.noreply.github.com>
2025-01-04 10:58:44 +0200

    Add MySQL Data Base

---
+++ backend/data_base/First_Part.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59a5353670c55d90895057588eafeb8520e5d66602f60362becfe4671c0ab8fc
+size 1265243507
---
+++ backend/data_base/Second_Part.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cada94a94a4d50efdad627ea1cc2f88900b53a4fe72108fa2841bcf7dff574ff
+size 1404649849

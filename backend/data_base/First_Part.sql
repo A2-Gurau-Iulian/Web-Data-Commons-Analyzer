46b17245554ef228dafe779e29ba14e68ff2491f
GurauIulian <79217309+A2-Gurau-Iulian@users.noreply.github.com>
2025-01-11 18:09:36 +0200

    MySql DataBase to Apache Jena

--- backend/data_base/First_Part.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:59a5353670c55d90895057588eafeb8520e5d66602f60362becfe4671c0ab8fc
-size 1265243507
--- backend/data_base/n_quads/test.py
+++
@@ -1,6 +0,0 @@
-import json
-a = "{\"longitude\": \"1.0577341E1\", \"latitude\": \"4.9294588E1\"}"
-
-b = json.loads(a)
-
-print(5/2)
